169da87f864d3679d9f4a0309d9ddc94ff8bceff
Umberto Picchini <umberto.picchini@gmail.com>
2017-11-03 16:50:07 +0100

    Update nonlingauss_errormodel.m

--- sample-matlab-code/nonlingauss/nonlingauss_errormodel.m
+++ sample-matlab-code/nonlingauss/nonlingauss_errormodel.m
@@ -1,12 +1,12 @@
 function yobssim = nonlingauss_errormodel(bigtheta,xhat,nobs,numsim)
 
-% produce Y, i.e. add measurement noise to X
-% produce nobs x numsim simulations of the X coordinate of the model.
+% simulates Y, i.e. adds measurement noise to X.
+%
 % bigtheta = vector of the model parameters (both free and fixed parameters).
 % xhat = a nobs x numsim matrix of of simulations from the latent state X
 % nobs = number of sampling times
 % numsim = number of simulations (=R in the paper).
-% yobssim= a nobs x numsim matrix of of simulations from the observable process Y
+% yobssim = a nobs x numsim matrix of of simulations from the observable process Y
 
 
 X0 = bigtheta(1);
